9ee4269436936c3eed62f347d41a6b6ce1da05eb
Colin Hart <me@colin-hart.com>
2014-07-18 17:11:51 -0400

    associations and inheritance in factories

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,19 +2,16 @@ require 'spec_helper'
 
 describe Phone do
   it "does not allow duplicate phone numbers per contcact" do
-    contact = Contact.create(firstname: 'Joe', lastname: 'Tester', email: 'joe@joetester.com')
-    contact.phones.create(phone_type: 'home', phone: '555-555-5555')
-    mobile_phone = contact.phones.build(phone_type: 'mobile', phone: '555-555-5555')
 
+    contact = create(:contact)
+    create(:home_phone, contact: contact, phone: '785-555-1234')
+
+    mobile_phone = build(:mobile_phone, contact: contact, phone: '785-555-1234')
     expect(mobile_phone).to have(1).errors_on(:phone)
   end
 
   it "allows two contacts to share a phone number" do
-    contact = Contact.create(firstname: 'Joe', lastname: 'Tester', email: 'joe@testerr.com')
-    contact.phones.create(phone_type: 'home', phone: '555-555-5555')
-    other_contact = Contact.new
-    other_phone = other_contact.phones.build(phone_type: 'home', phone: '555-555-5555')
-
-    expect(other_phone).to be_valid
+    create(:home_phone, phone: "785-555-1234")
+    expect(build(:home_phone, phone: "785-555-1234")).to be_valid
   end
 end
